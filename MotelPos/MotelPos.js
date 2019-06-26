d9c4c6219a4682e1b243b5c3a8b18bb076df6c85
James Harris <dev@jharris.nz>
2019-06-26 15:40:35 +1000

    Update version to 2.6.0

--- MotelPos/MotelPos.js
+++ MotelPos/MotelPos.js
@@ -40,7 +40,7 @@ export class MotelPos
         this._posId = "MOTELPOS1";
         this._eftposAddress = "192.168.1.1";
         this._spiSecrets = null;
-        this._version = '2.5.0';
+        this._version = '2.6.0';
         this._serialNumber = "";
         this._rcpt_from_eftpos = false;
         this._sig_flow_from_eftpos = false;
