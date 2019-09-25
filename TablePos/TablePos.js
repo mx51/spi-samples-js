49320f32b424b2fd46d3c597982608a0795c9711
Matt Gresham <mgresham@assemblypayments.com>
2019-09-26 09:01:48 +1000

    feat: Upgrade SPI library to 2.6.3

--- TablePos/TablePos.js
+++ TablePos/TablePos.js
@@ -40,7 +40,7 @@ class TablePos
         this._posId = "TABLEPOS1";
         this._eftposAddress = "192.168.1.1";
         this._spiSecrets = null;
-        this._version = '2.6.0';
+        this._version = '2.6.3';
         this._serialNumber = "";
         this._rcpt_from_eftpos = false;
         this._sig_flow_from_eftpos = false;
@@ -650,6 +650,7 @@ class TablePos
                         input.checked = true;
                     });
 
+                    localStorage.removeItem('pat_config');
                     this.EnablePayAtTableConfigs();
                     this._pat.PushPayAtTableConfig();
                 },
