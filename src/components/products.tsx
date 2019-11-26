72dec0db838050feb4830cbeb26c70da0f586017
Gopiben Patel <gpatel@assemblypayments.com>
2019-11-26 11:56:56 +1100

    WIP-order

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './products.css';
 import { Col, Row } from 'react-bootstrap';
+import { Icon } from '@iconify/react';
+import trashAlt from '@iconify/icons-fa-regular/trash-alt';
 
 type Products = Array<Category>;
 type Category = {
@@ -11,10 +13,28 @@ type Product = {
   id: string;
   name: string;
   image: string;
+  price: string;
 };
 
-const handleProductClick = (id: Number) => {
+const handleProductClick = (
+  id: string,
+  allProducts: Products,
+  shortlistedProducts: any,
+  updateShortlistedProducts: any
+) => {
   console.log(`clicked ... ${id}`);
+  let clickedProduct;
+  allProducts.forEach(c =>
+    c.list.forEach(p => {
+      if (p.id === id) {
+        clickedProduct = p;
+      }
+    })
+  );
+  console.log(clickedProduct);
+  const newList: any = [clickedProduct, ...shortlistedProducts];
+  updateShortlistedProducts(newList);
+  console.log(newList);
 };
 
 function Products() {
@@ -26,16 +46,19 @@ function Products() {
           id: '101',
           name: 'Chicken burger',
           image: `./images/chicken.jpg`,
+          price: '12',
         },
         {
           id: '102',
           name: 'Beef burger',
           image: './images/beef.png',
+          price: '12',
         },
         {
           id: '103',
           name: 'Veggie burger',
           image: './images/veggie.jpg',
+          price: '12',
         },
       ],
     },
@@ -46,11 +69,13 @@ function Products() {
           id: '201',
           name: 'Veggie Salad',
           image: './images/caesar.jpg',
+          price: '7',
         },
         {
           id: '202',
           name: 'Caeser Salad',
           image: './images/caesar.jpg',
+          price: '7',
         },
       ],
     },
@@ -61,38 +86,73 @@ function Products() {
           id: '301',
           name: 'Coke',
           image: './images/pepsi.jpg',
+          price: '4',
         },
       ],
     },
   ];
+  const [shortlistedProducts, updateShortlistedProducts] = useState([]);
+
+  function handleRemoveProduct(id: any) {
+    console.log('Remove product', id);
+
+    const newArr: any = [];
+
+    let removed: Boolean = false;
+    shortlistedProducts.forEach((item: Product) => {
+      if (item.id === id && !removed) {
+        removed = true;
+      } else {
+        newArr.push(item);
+      }
+    });
+
+    updateShortlistedProducts(newArr);
+  }
 
   return (
     <>
       <Row>
-        <Col sm={8}>
+        <Col lg={8}>
           {allProducts.map(cat => (
-            <ProductCategory category={cat} onProductClick={handleProductClick} />
+            <ProductCategory
+              category={cat}
+              onProductClick={handleProductClick}
+              allProducts={allProducts}
+              shortlistedProducts={shortlistedProducts}
+              updateShortlistedProducts={updateShortlistedProducts}
+            />
           ))}
         </Col>
-        <Col sm={4} className="order-sidebar">
-          <h1 className="order-header">Order</h1>
+        <Col lg={4} className="order-sidebar">
+          <Order list={shortlistedProducts} onRemoveProduct={handleRemoveProduct} />
         </Col>
       </Row>
     </>
   );
 }
 
-function ProductCategory(props: { category: Category; onProductClick: Function }) {
-  const { category } = props;
-  const { onProductClick } = props;
+function ProductCategory(props: {
+  category: Category;
+  onProductClick: Function;
+  allProducts: any;
+  shortlistedProducts: any;
+  updateShortlistedProducts: any;
+}) {
+  const { category, onProductClick, allProducts, shortlistedProducts, updateShortlistedProducts } = props;
   return (
     <div>
       <h3 className="product-category">{category.categoryName}</h3>
       <hr />
       {category.list.map(p => (
         <>
-          <button type="button" className="product-display" onClick={() => onProductClick(p.id)}>
-            <img src={p.image} alt={p.name} height="120" width="130" />
+          <button
+            key={p.id}
+            type="button"
+            className="product-display"
+            onClick={() => onProductClick(p.id, allProducts, shortlistedProducts, updateShortlistedProducts)}
+          >
+            <img src={p.image} alt={p.name} />
             <p> {p.name} </p>
           </button>
         </>
@@ -101,4 +161,42 @@ function ProductCategory(props: { category: Category; onProductClick: Function }
   );
 }
 
+function Order(props: { list: any; onRemoveProduct: Function }) {
+  const { list } = props;
+  const groupedProducts: any = [];
+
+  list.forEach((item: Product) => {
+    if (!groupedProducts[item.id]) {
+      groupedProducts[item.id] = { ...item, count: 1 };
+    } else {
+      groupedProducts[item.id] = { ...item, count: groupedProducts[item.id].count + 1 };
+    }
+  });
+
+  console.log('Grouped: ', groupedProducts);
+
+  return (
+    <div>
+      <h2 className="order-header">Order</h2>
+      <ul className="nobull">
+        {groupedProducts.map((item: any) => (
+          <li className="space" key={item.id}>
+            <button className="orderList" type="button">
+              <Row>
+                <Col sm={1}>{item.count} </Col>
+                <Col sm={1}> X </Col>
+                <Col sm={5}>{item.name}</Col>
+                <Col sm={2}>${item.price}.00</Col>
+                <Col sm={2}>${item.price * item.count}.00</Col>
+                <Col sm={1}>
+                  <Icon icon={trashAlt} />
+                </Col>
+              </Row>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 export default Products;
