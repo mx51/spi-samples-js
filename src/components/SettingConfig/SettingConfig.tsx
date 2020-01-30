ea89be6435de3bd086ce3124e7f61701b3b93272
Gopiben Patel <gpatel@assemblypayments.com>
2020-01-30 11:24:26 +1100

    print merchant copy working

--- src/components/SettingConfig/SettingConfig.tsx
+++ src/components/SettingConfig/SettingConfig.tsx
@@ -1,16 +1,31 @@
 import React, { useState, SyntheticEvent } from 'react';
 import { Form } from 'react-bootstrap';
-
 import Textarea from '../Input/Textarea';
+import { terminalStatus as terminalStatusService } from '../../services';
 
 function SettingConfig(props: { spi: any }) {
   const { spi } = props;
   const [sigFlow, setSigFlow] = useState(window.localStorage.getItem('sig_flow_from_eftpos') === 'true');
+  const [eftposReceipt, setEftposReceipt] = useState(window.localStorage.getItem('rcpt_from_eftpos') === 'true');
+  const [printMerchantCopy, setPrintMerchantCopy] = useState(
+    window.localStorage.getItem('print_merchant_copy_input') === 'true'
+  );
   return (
     <div>
       <h2 className="sub-header">Setting</h2>
       <div className="mr-3 ml-3">
-        <Form.Check type="checkbox" id="check-receipt-eftpos" label="Receipt from EFTPOS" />
+        <Form.Check
+          type="checkbox"
+          id="check-receipt-eftpos"
+          label="Receipt from EFTPOS"
+          checked={eftposReceipt}
+          onChange={(e: SyntheticEvent<HTMLInputElement>) => {
+            if (e && e.currentTarget) {
+              setEftposReceipt(e.currentTarget.checked);
+              console.log(sigFlow);
+            }
+          }}
+        />
         <Form.Check
           type="checkbox"
           id="check-sig-eftpos"
@@ -23,15 +38,30 @@ function SettingConfig(props: { spi: any }) {
             }
           }}
         />
-        <Form.Check type="checkbox" id="print-merchant-copy" label="Print Merchant Copy" />
+        <Form.Check
+          type="checkbox"
+          id="print-merchant-copy"
+          label="Print Merchant Copy"
+          checked={printMerchantCopy}
+          onChange={(e: SyntheticEvent<HTMLInputElement>) => {
+            if (e && e.currentTarget) {
+              setPrintMerchantCopy(e.currentTarget.checked);
+              console.log(printMerchantCopy);
+            }
+          }}
+        />
         <Textarea id="Receipt header" name="Receipt header" label="Receipt header" />
         <Textarea id="Receipt footer" name="Receipt footer" label="Receipt footer" />
         <button
           type="button"
           className="primary-button"
           onClick={() => {
+            spi.Config.PromptForCustomerCopyOnEftpos = eftposReceipt;
             spi.Config.SignatureFlowOnEftpos = sigFlow;
+            terminalStatusService.setIsMerchantReceiptPrinted({ Info: () => {}, Clear: () => {} }, spi, true, () => {});
+            window.localStorage.setItem('rcpt_from_eftpos', eftposReceipt.toString());
             window.localStorage.setItem('check-sig-eftpos', sigFlow.toString());
+            window.localStorage.setItem('print-merchant-copy', printMerchantCopy.toString());
           }}
         >
           Save & Apply
