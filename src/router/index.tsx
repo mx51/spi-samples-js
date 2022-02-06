b84cf4df52db7b343e7faf8029f69646a8bba16f
Gopiben Patel <gopi@mx51.io>
2022-02-07 05:02:02 +0530

    Remove preAuth P@T and fleet setting

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,11 +2,11 @@ import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Loading from '../components/Loading';
 import {
-  PATH_FLEET_SETTINGS,
+  // PATH_FLEET_SETTINGS,
   PATH_PAIR,
-  PATH_PAY_AT_TABLE,
+  // PATH_PAY_AT_TABLE,
   PATH_PAY_NOW,
-  PATH_PRE_AUTH,
+  // PATH_PRE_AUTH,
   PATH_PURCHASE,
   PATH_REFUND,
   PATH_TERMINALS,
@@ -14,10 +14,10 @@ import {
   PATH_CASH_OUT,
 } from '../definitions/constants/routerConfigs';
 
-const FleetSettings = React.lazy(() => import('../components/FleetSettingsPage'));
+// const FleetSettings = React.lazy(() => import('../components/FleetSettingsPage'));
 const Pair = React.lazy(() => import('../components/PairPage'));
-const PayAtTable = React.lazy(() => import('../components/PayAtTablePage'));
-const PreAuth = React.lazy(() => import('../components/PreAuthPage'));
+// const PayAtTable = React.lazy(() => import('../components/PayAtTablePage'));
+// const PreAuth = React.lazy(() => import('../components/PreAuthPage'));
 const Purchase = React.lazy(() => import('../components/PurchasePage'));
 const Refund = React.lazy(() => import('../components/RefundPage'));
 const Terminals = React.lazy(() => import('../components/TerminalsPage'));
@@ -31,10 +31,10 @@ const AppRoutes = (): React.ReactElement => (
   <Router>
     <Suspense fallback={<Loading />}>
       <Switch>
-        <Route exact path={PATH_FLEET_SETTINGS} component={FleetSettings} />
+        {/* <Route exact path={PATH_FLEET_SETTINGS} component={FleetSettings} /> */}
         <Route exact path={PATH_PAIR} component={Pair} />
-        <Route exact path={PATH_PAY_AT_TABLE} component={PayAtTable} />
-        <Route exact path={PATH_PRE_AUTH} component={PreAuth} />
+        {/* <Route exact path={PATH_PAY_AT_TABLE} component={PayAtTable} /> */}
+        {/* <Route exact path={PATH_PRE_AUTH} component={PreAuth} /> */}
         <Route exact path={PATH_PURCHASE} component={Purchase} />
         <Route exact path={PATH_REFUND} component={Refund} />
         <Route exact path={PATH_TERMINALS} component={Terminals} />
