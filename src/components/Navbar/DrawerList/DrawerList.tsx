040307cc632d2f7ad6473b51e6dfdd5243aef1e2
jwsandeman <jw.sandeman@gmail.com>
2023-07-05 21:55:14 +1000

    INTG-544 Added EspressoPOS version to menu

--- src/components/Navbar/DrawerList/DrawerList.tsx
+++ src/components/Navbar/DrawerList/DrawerList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
+import packageJson from '../../../../package.json';
 import menuItems from '../../../definitions/constants/menuItemsConfigs';
 import { ReactComponent as SecondaryLogoIcon } from '../../../images/SecondaryLogoIcon.svg';
 import { IDrawerList } from '../interfaces/NavbarInterfaces';
@@ -12,6 +13,7 @@ import MenuItems from './MenuItems';
 function DrawerList({ toggleDrawer }: IDrawerList): React.ReactElement {
   const classes = useStyles();
   const { samplePos, terminals, support } = menuItems;
+  const packageVersion: string = packageJson.version;
 
   return (
     <Box className={classes.list} onClick={toggleDrawer} onKeyDown={toggleDrawer}>
@@ -26,6 +28,8 @@ function DrawerList({ toggleDrawer }: IDrawerList): React.ReactElement {
         </div>
         <div>
           <MenuItems menuItems={support} />
+          <Divider />
+          <Typography className={classes.versionText}>Version {packageVersion}</Typography>
         </div>
       </div>
     </Box>
