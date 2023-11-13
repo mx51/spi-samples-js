7c1fb59dae8f00408f201e2ce6ebe8e964e70a6b
Rossi Le <rossi@mx51.io>
2023-11-13 11:38:01 +1100

    Redirect to espresso vs dev espresso

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -4,23 +4,27 @@ import { getSpiVersion } from '../../services/_common/uiHelpers';
 
 import './App.scss';
 
+const espressoUrl = window.location.origin.includes('rebuild')
+  ? 'https://dev.espressopos.mx51.io'
+  : 'https://espressopos.mx51.io';
+
 const App = () => (
   <div className="app">
     <h1 className="bpos-heading">Choose your SPI {getSpiVersion()} POS Sample</h1>
     <nav className="homePageLink">
       <ol>
         <li>
-          <a target="_blank" rel="noreferrer" href="https://espressopos.mx51.io">
+          <a target="_blank" rel="noreferrer" href={espressoUrl}>
             Burger POS
           </a>
         </li>
         <li>
-          <a target="_blank" rel="noreferrer" href="https://espressopos.mx51.io/">
+          <a target="_blank" rel="noreferrer" href={espressoUrl}>
             Ramen POS
           </a>
         </li>
         <li>
-          <a target="_blank" rel="noreferrer" href="https://espressopos.mx51.io/">
+          <a target="_blank" rel="noreferrer" href={espressoUrl}>
             Motel POS
           </a>
         </li>
