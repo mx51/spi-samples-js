88fbedd331e639ac34594cd0c22e8435ace241c8
Ruoxin Wang <ruoxin@mx51.io>
2024-03-08 16:09:31 +1100

    use useReducer

--- src/components/SplitPage/SplitPage.tsx
+++ src/components/SplitPage/SplitPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { orderPromptForCashoutSelector, orderTotalSelector } from '../../redux/reducers/ProductSlice/productSelector';
@@ -22,6 +22,21 @@ import { clearAllProducts } from '../../redux/reducers/ProductSlice/productSlice
 
 type Stage = 'initialSplit' | 'splitConfirmation' | 'splitReceipt';
 
+type State = {
+  stage: Stage;
+  splitMode: SplitMode;
+  splitArray: number[];
+  currentSplitNumber: number;
+  outstandingAmount: number;
+  showModal?: boolean;
+};
+
+type Action =
+  | { type: 'startSplit'; splitMode: SplitMode; splitNumber: number }
+  | { type: 'startTransaction' }
+  | { type: 'showReceipt' }
+  | { type: 'nextSplit' };
+
 function calculateSplitArray(totalAmount: number, splitNumber: number): number[] {
   if (splitNumber === 1) {
     return [totalAmount];
@@ -33,8 +48,9 @@ function calculateSplitArray(totalAmount: number, splitNumber: number): number[]
 }
 
 export const SplitPage: React.FC = () => {
-  const dispatch = useDispatch();
+  const appDispatch = useDispatch();
   const history = useHistory();
+
   const totalAmount = useSelector(orderTotalSelector);
   const promptForCashout = useSelector(orderPromptForCashoutSelector);
 
@@ -43,76 +59,108 @@ export const SplitPage: React.FC = () => {
   const isTxFinished = useSelector(terminalTxFlowFinishedTracker(selectedTerminalId)) ?? false;
   const isTxSuccess = useSelector(isTerminalTxFlowSuccess(selectedTerminalId));
 
-  const [currentSplitNumber, setCurrentSplitNumber] = useState(0);
-  const [splitArray, setSplitArray] = useState<number[]>([]);
-  const [outstandingAmount, setOutstandingAmount] = useState(0);
-
-  const [stage, setStage] = useState<Stage>('initialSplit');
-  const [showModal, setShowModal] = useState(false);
-
   const backToPurchase = () => {
     history.push(PATH_PURCHASE);
   };
 
-  const handleInitialClick = (splitMode: SplitMode, splitNumber: number) => {
-    setCurrentSplitNumber(0);
-    setOutstandingAmount(totalAmount);
-    if (splitMode === 'splitEvenly') {
-      setSplitArray(calculateSplitArray(totalAmount, splitNumber));
+  const reducer = (currentState: State, action: Action): State => {
+    if (action.type === 'startSplit') {
+      if (action.splitMode === 'splitEvenly') {
+        return {
+          stage: 'splitConfirmation',
+          splitMode: action.splitMode,
+          splitArray: calculateSplitArray(totalAmount, action.splitNumber),
+          currentSplitNumber: 0,
+          outstandingAmount: totalAmount,
+        };
+      }
+      return currentState;
     }
-    setStage('splitConfirmation');
-  };
+    if (action.type === 'startTransaction') {
+      const splitAmount = currentState.splitArray[currentState.currentSplitNumber];
+      initiatePurchase(selectedTerminalId, splitAmount, 0, 0, 0, promptForCashout);
 
-  const handleConfirmationClickNext = () => {
-    setShowModal(true);
-    initiatePurchase(selectedTerminalId, splitArray[currentSplitNumber], 0, 0, 0, promptForCashout);
-  };
-
-  const handleReceiptClickNext = () => {
-    if (currentSplitNumber >= splitArray.length - 1) {
-      dispatch(clearAllProducts());
-      backToPurchase();
+      return {
+        ...currentState,
+        showModal: true,
+      };
     }
-    setCurrentSplitNumber(currentSplitNumber + 1);
-    setStage('splitConfirmation');
-  };
+    if (action.type === 'showReceipt') {
+      const splitAmount = currentState.splitArray[currentState.currentSplitNumber];
 
-  const handleModalTransactionDone = () => {
-    if (isTxSuccess) {
-      setOutstandingAmount(outstandingAmount - splitArray[currentSplitNumber]);
-      setShowModal(false);
-      setStage('splitReceipt');
-    } else {
-      backToPurchase();
+      return {
+        ...currentState,
+        stage: 'splitReceipt',
+        outstandingAmount: currentState.outstandingAmount - splitAmount,
+        showModal: false,
+      };
     }
+    if (action.type === 'nextSplit') {
+      if (currentState.currentSplitNumber >= currentState.splitArray.length - 1) {
+        appDispatch(clearAllProducts());
+        backToPurchase();
+      }
+      return {
+        ...currentState,
+        stage: 'splitConfirmation',
+        currentSplitNumber: currentState.currentSplitNumber + 1,
+      };
+    }
+    return currentState;
   };
+  const [state, dispatch] = useReducer(reducer, {
+    stage: 'initialSplit',
+    splitMode: 'splitEvenly',
+    splitArray: [],
+    currentSplitNumber: 0,
+    outstandingAmount: 0,
+  });
 
   return (
     <Layout>
-      {stage === 'initialSplit' && <InitialSplitPanel totalAmount={totalAmount} onClickNext={handleInitialClick} />}
-
-      {stage === 'splitConfirmation' && (
-        <SplitConfirmationPanel
-          splitNumber={currentSplitNumber}
-          splitAmount={splitArray[currentSplitNumber]}
-          onClickNext={handleConfirmationClickNext}
+      {state.stage === 'initialSplit' && (
+        <InitialSplitPanel
+          totalAmount={totalAmount}
+          onClickNext={(splitMode, splitNumber) => {
+            dispatch({
+              type: 'startSplit',
+              splitMode,
+              splitNumber,
+            });
+          }}
         />
       )}
 
-      {stage === 'splitReceipt' && (
+      {state.stage === 'splitConfirmation' && (
+        <SplitConfirmationPanel
+          splitNumber={state.currentSplitNumber}
+          splitAmount={state.splitArray[state.currentSplitNumber]}
+          onClickNext={() => {
+            dispatch({
+              type: 'startTransaction',
+            });
+          }}
+        />
+      )}
+
+      {state.stage === 'splitReceipt' && (
         <SplitReceiptPanel
           currentTerminal={selectedTerminal}
           isTxSuccess={isTxSuccess}
           isTxFinished={isTxFinished}
-          currentSplitNumber={currentSplitNumber}
-          totalSplitNumber={splitArray.length}
-          amount={splitArray[currentSplitNumber]}
-          outstandingAmount={outstandingAmount}
-          onClickNext={handleReceiptClickNext}
+          currentSplitNumber={state.currentSplitNumber}
+          totalSplitNumber={state.splitArray.length}
+          amount={state.splitArray[state.currentSplitNumber]}
+          outstandingAmount={state.outstandingAmount}
+          onClickNext={() => {
+            dispatch({
+              type: 'nextSplit',
+            });
+          }}
         />
       )}
 
-      {showModal && (
+      {state.showModal && (
         <TransactionProgressModal
           terminalId={selectedTerminalId}
           transactionType={SPI_TRANSACTION_TYPES.Purchase}
@@ -124,9 +172,17 @@ export const SplitPage: React.FC = () => {
             backToPurchase();
           }}
           onRetryTransaction={() => {
-            initiatePurchase(selectedTerminalId, splitArray[currentSplitNumber], 0, 0, 0, promptForCashout);
+            initiatePurchase(selectedTerminalId, state.splitArray[state.currentSplitNumber], 0, 0, 0, promptForCashout);
           }}
-          onDone={handleModalTransactionDone}
+          onDone={() => {
+            if (isTxSuccess) {
+              dispatch({
+                type: 'showReceipt',
+              });
+            } else {
+              backToPurchase();
+            }
+          }}
         />
       )}
     </Layout>
