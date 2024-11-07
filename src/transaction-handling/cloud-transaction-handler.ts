745a0a17bac227a7f3ce9b6ba4d68a699a58737c
hayden-mx51 <hayden@mx51.io>
2024-11-07 13:55:37 +1100

    [FE-20] Use common request signing

--- src/transaction-handling/cloud-transaction-handler.ts
+++ src/transaction-handling/cloud-transaction-handler.ts
@@ -1,13 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import CryptoJS from 'crypto-js';
 import { TransactionHandler } from './transaction-handler';
 import { SpiCloudPairing } from '../redux/reducers/PairingSlice/interfaces';
 import { selectCloudPairing } from '../redux/reducers/PairingSlice/pairingSelectors';
 import { ITerminalReceiptFormatProps, ITxFlow } from '../redux/reducers/TerminalSlice/interfaces';
+import { signedRequestInit } from '../utils/common/signRequest';
 
 /* eslint-disable class-methods-use-this */
-/* eslint-disable no-console */ // TODO: Remove this
 /* eslint-disable @typescript-eslint/no-unused-vars */ // TODO: Remove this
 export class CloudTransactionHandler extends TransactionHandler<SpiCloudPairing> {
   get terminalId(): string | undefined {
@@ -36,54 +33,32 @@ export class CloudTransactionHandler extends TransactionHandler<SpiCloudPairing>
     surchargeAmount: number,
     promptForCashout: boolean
   ): Promise<void> {
+    // TODO: Support cashout
+
+    const { keyId, signingSecret, spiCloudApiBaseUrl } = this.terminal!;
+    const url = `${spiCloudApiBaseUrl}/api/v1/transactions`;
+    const requestBody = {
+      purchase_details: {
+        purchase_amount: purchaseAmount,
+        surcharge_amount: surchargeAmount,
+        tip_amount: tipAmount,
+      },
+    };
+
     try {
-      // TODO: Throw if no terminal?
+      const signedRequest = await signedRequestInit(keyId, signingSecret, url, 'POST', requestBody);
+      const response = await fetch(url, signedRequest);
 
-      const { keyId, signingSecret, spiCloudApiBaseUrl } = this.terminal!;
-      const url = new URL(`${spiCloudApiBaseUrl}/api/v1/transactions`);
-      const now = Math.floor(Date.now() / 1000);
-      const method = 'POST';
-      const requestBody = {
-        purchase_details: {
-          purchase_amount: purchaseAmount,
-          tip_amount: tipAmount,
-          surcharge_amount: surchargeAmount,
-        },
-      };
-
-      const signatureInput = `("@method" "@authority" "@request-target" "content-digest");created=${now};alg="hmac-sha256";keyid="${keyId}"`;
-      const contentDigest = CryptoJS.SHA256(JSON.stringify(requestBody)).toString(CryptoJS.enc.Base64);
-      // Line breaks are required for the signature to be valid
-      const dataToEncrypt =
-        `"@method": ${method}` +
-        '\n' +
-        `"@authority": ${url.host}` +
-        '\n' +
-        `"@request-target": ${url.pathname}` +
-        '\n' +
-        `"content-digest": sha-256=:${contentDigest}:` +
-        '\n' +
-        `"@signature-params": ${signatureInput}`;
-      const signature = CryptoJS.HmacSHA256(dataToEncrypt, signingSecret).toString(CryptoJS.enc.Base64);
-
-      const response = await fetch(url, {
-        method,
-        headers: {
-          'Content-Type': 'application/json',
-          Signature: `sig1=:${signature}:`,
-          'Signature-Input': `sig1=${signatureInput}`,
-          'Content-Digest': `sha-256=:${contentDigest}:`,
-        },
-        body: JSON.stringify(requestBody),
-      });
       if (!response.ok) {
-        throw new Error(response.statusText);
+        // TODO: Handle errors
+        // return; // TODO: Uncomment
       }
-      const result = await response.json();
-      console.log('Txn result', result);
+
+      // TODO: Start polling for transaction status
     } catch (error) {
+      // TODO: Handle errors
       // eslint-disable-next-line no-console
-      console.error('Error pairing', error);
+      console.error('Error initiating purchase', error);
     }
   }
 
